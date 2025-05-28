74be05a7c5321d50f3bf0d6f3726ac67555d6d46
Abstha002 <ab.stha002@proton.me>
2025-05-28 14:48:42 +0545

    cloudinary multer

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,10 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+   
+   // Configuration
+    cloudinary.config({ 
+        cloud_name: 'dduquh8vb', 
+        api_key: '878818315442615', 
+        api_secret: 'Di-5YfNVrWjcfzJkTtUSBr_eHjo' // Click 'View API Keys' above to copy your API secret
+    });
+    
